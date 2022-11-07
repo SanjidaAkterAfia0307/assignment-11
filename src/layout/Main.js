2a96e9589dedc9a96e97c07b8fd1ce9419b83b0a
Afia <sanjidaakterafia0307@gmail,com>
2022-11-08 02:32:16 +0600

    header done

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-
+import Header from "../components/Shared/Header/Header";
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,10 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'
+
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('tw-elements/dist/plugin')
-  ],
+  plugins: [require("daisyui")],
 }
